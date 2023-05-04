2b4dc32315efc5b0472a62da15fe5640b77d6c27
pedroheck <pedroscheck@hotmail.com>
2023-05-03 21:58:43 -0300

    test

--- python_names_storage.py
+++ python_names_storage.py
@@ -3,7 +3,6 @@ from google.cloud import storage
 from google.oauth2 import service_account
 import requests
 
-
 headers = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
